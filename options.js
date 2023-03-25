b755a690cb526a0a084ccf2a2d00fa234fd13800
BlankaPeller <67928730+BlankaPeller@users.noreply.github.com>
2023-03-24 21:18:43 -0400

    Add files via upload

---
+++ options.js
@@ -0,0 +1,45 @@
+var Orders = [];
+function getValue() {
+    var Size = document.getElementsByName('size');
+    var SizeSelected = ""
+    var Milk = document.getElementsByName('milk');
+    var MilkSelected = ""
+    var Sweetener = document.getElementsByName('sweetener');
+    var SweetenerSelected = ""
+    var SweetAmount = document.getElementById("sweetamount");
+    var Temp = document.getElementsByName('temperature');
+    var TempSelected = ""
+
+    for (var i = 0, length = Size.length; i < length; i++) {
+        if (Size[i].checked) {
+            SizeSelected = Size[i].value;
+            break;
+        }
+    }
+    for (var i = 0, length = Milk.length; i < length; i++) {
+        if (Milk[i].checked) {
+            MilkSelected = Milk[i].value;
+            break;
+        }
+    }
+    for (var i = 0, length = Sweetener.length; i < length; i++) {
+        if (Sweetener[i].checked) {
+            SweetenerSelected = Sweetener[i].value;
+            break;
+        }
+    }
+    for (var i = 0, length = Temp.length; i < length; i++) {
+        if (Temp[i].checked) {
+            TempSelected = Temp[i].value;
+            break;
+        }
+    }
+    var order = [SizeSelected, MilkSelected, SweetenerSelected, SweetAmount.value, TempSelected];
+    var options = document.querySelectorAll('input[type=checkbox]:checked');
+    for (var i = 0; i < options.length; i++) {
+        order.push(options[i].value);
+    }
+    Orders.push(order);
+    document.getElementById("result").innerHTML = Orders[0];
+}
+SessionStorage.setItem("Orders", JSON.stringify(Orders));
